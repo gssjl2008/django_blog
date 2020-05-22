89a4b04dd0ab7184426e60fb2a713af653efa0f4
gssjl2008 <gssjl2004@126.com>
2020-05-22 10:08:43 +0800

    使用 importlib 来自动注册models

--- dj_demo/blog/admin.py
+++ dj_demo/blog/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
 
 # Register your models here.
 
-from .models import Author, Article, Tag, Category, Menu
+# from .models import Author, Article, Tag, Category, Menu
+# #
+# for cls in [Article, Author, Tag, Category, Menu]:
+#     print(f"cls: {cls}")
+#     admin.site.register(cls)
+# 动态增加models中的类
+import importlib
 
-for cls in [Article, Author, Tag, Category, Menu]:
-    admin.site.register(cls)
+clses = importlib.import_module('blog.models')
+
+for cls in dir(clses):
+    cls = getattr(clses, cls, None)
+    if isinstance(cls, type):
+        try:
+            admin.site.register(cls)
+        except AlreadyRegistered:
+            pass
